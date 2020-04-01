87aaaf52cd671e91ead399df2dc3dd91a2830ca1
Joel Campbell <joel@joelcampbell.com>
2020-04-01 09:43:56 -0400

    Paramaterized minecraft class

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,15 +1,17 @@
 class minecraft {
-  file {'/opt/minecraft':
+  $install_dir = '/opt/minecraft'
+  file {$install_dir:
     ensure => directory,
   }
-  file {'/opt/minecraft/server.jar':
+  file {"${install_dir}/server.jar":
     ensure => file,
     source => 'puppet:///modules/minecraft/server.jar',
+    before => Service['minecraft'],
   }
   package { 'java':
     ensure => present,
   }
-  file { '/opt/minecraft/eula.txt':
+  file {"${install_dir}/eula.txt":
     ensure  => file,
     content => 'eula=true',
   }
@@ -18,7 +20,8 @@ class minecraft {
     source => 'puppet:///modules/minecraft/minecraft.service',
   }
   service { 'minecraft':
-    ensure => running,
-    enable => true,
+    ensure  => running,
+    enable  => true,
+    require => [Package['java'],File["${install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']],
   }
 }
